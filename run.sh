620283d7349cc0df1b38cab8492b00fd2555ece5
The_Flash <90428057+Savitar44@users.noreply.github.com>
2021-09-26 18:12:30 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ echo -e "\033[1;32mINSTALANDO OPENSSH...\033[0m"
 sleep 2s
 pkg install openssh
 echo -e "\033[1;32mINSTALANDO DROPBEAR...\033[0m"
+echo -e
 sleep 2s
 pkg install dropbear
 clear
+curl -sO https://raw.githubusercontent.com/Savitar44/ssh/main/menu && chmod 777 menu && ./menu
